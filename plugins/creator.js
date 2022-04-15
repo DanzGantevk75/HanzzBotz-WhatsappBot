71ba9318239525176221caef5b7701c00b93318b
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-16 01:23:18 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,13 +1,13 @@
 let handler = function (m) {
 	let caption = `
-	Ini Owner Ku Kak (Pemilik Bot)
+	Ini Owner Ku Kak (Danzz)
 -Bukan Bot
 -Awali Chat Dengan Salam Atau Sapa
 -P Tidak Akan Dijawab
 -Tidak Menerima Save Contact
 `.trim()
-  // this.sendContact(m.chat, '6283144570377', 'Raku Gans', m)
-  this.sendContact(m.chat, '6281212083267', 'My Owner', m)
+  // this.sendContact(m.chat, '6288270863279', 'Danzz', m)
+  this.sendContact(m.chat, '6288270863279', 'My Owner', m)
   conn.send3Button(m.chat, caption, 'Mau Tanya Apa?', 'Sc', '.sc', 'Sewa Bot', '.sewabot', 'Status Owner', '.infoowner', m)
   
 }
