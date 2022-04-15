a3c24d5812225c63561fd7ef17ecadb7e814ff93
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-16 01:42:07 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -2,11 +2,11 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 *BOT Ini Menggunakan SC Dari : https://github.com/Nurutomo/wabot-aq
 Lalu Di Recode Oleh
-@6287834993722 (Nathan)
+@6288270863279 (Danzz)
 @6281212083267 (Hanzz)
 `.trim()
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *Sc By Hanzz-kun* 🔥', 'status@broadcast')
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *Sc By Danzz* 🔥', 'status@broadcast')
 }
 handler.help = ['sc']
 handler.tags = ['info']
