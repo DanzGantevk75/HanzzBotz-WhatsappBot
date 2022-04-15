f314a001637abe6f5ebaefc3f4d1a0359d7b11cd
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-16 01:27:26 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn, text }) => {
   let ext= `
 *DONASI*
 
-*TSEL* : 081387127465
-*GOPAY* : 081387127465
-*DANA* : 081387127465
-*SAWERIA* : saweria.co/HanzzBotz
+*PULSA* : 088270863279
+*GOPAY* : 088270863279
+*DANA* : 088270863279
+*SAWERIA* : -
 
 `.trim()
-await conn.sendButtonLoc(m.chat, await (await fetch(`https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Donasi`)).buffer(), ext.trim(), 'HanzzBotz By Hanzz-kun', '🙋‍♂️ NOMER OWNER', '.owner',m)
+await conn.sendButtonLoc(m.chat, await (await fetch(`https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Donasi`)).buffer(), ext.trim(), 'DanzBot', '🙋‍♂️ NOMER OWNER', '.owner',m)
     
 
 }
